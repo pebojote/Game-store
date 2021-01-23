2bdf79b27986fbe3cd9412559dadb4b6d67e2ddb
workpebojot <pebojot.edison.work@outlook.com>
2021-01-23 18:57:36 +0800

    Remove Unnecessary Syntax
    See: #6

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php require './source/config/routes.php'; ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>
+		<?php echo $title; ?>
+	</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="./source/assets/styles/<?php echo $styles; ?>">
+</head>
+
+<body>
+	<?php echo $content; ?>
+	<script src="./source/assets/scripts/<?php echo $scripts; ?>"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</body>
+
+</html>
